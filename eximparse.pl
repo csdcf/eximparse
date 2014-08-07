162c3c70c2a50bd7ebe94a04949cd2a3fd7caa95
Al Biheiri <al@forgottheaddress.com>
2014-08-07 15:32:33 -0700

    progress i guess

--- eximparse.pl
+++ eximparse.pl
@@ -1,4 +1,4 @@
-#!//usr/bin/env perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
@@ -12,20 +12,37 @@ use Data::Dumper;
 #my $filename = "/Users/abiheiri/Downloads/tmp.abiheiri/maillog.1";
 #open my $fh, "<", $filename or die "could not open $filename: $!";
 
-my %count_reject;
-my %count_spamhaus;
 
-my %counts;
+my %c;
+
+print "Generating a report, hold your horses...\n";
 
 while (<>)
 {
-        for my $match (/( rejected|spamhaus)/g) {
-		$counts{$match}++
+        for my $match (/( rejected|spamhaus|unsolicited|rate limited|=>|<=)/g) {
+		$c{$match}++
 	}
 }
 
-print
-        "-" x 20, "+", "-" x 40, "\n",
-	"Total rejected mail | Total rejected ala spamhaus.org\n",
-        "-" x 20, "+", "-" x 40, "\n",
-	sprintf "%19d | %39d\n", $counts{" rejected"}, $counts{spamhaus};	
+#print
+#        "-" x 20, "+", "-" x 40,"\n",
+#	"Total rejected mail | Total rejected thanks to spamhaus.org\n",
+#        "-" x 20, "+", "-" x 40,"\n",
+#	sprintf "%19d | %39d \n", $counts{" rejected"}, $counts{spamhaus};	
+
+print <<EOF
+
+|================================|
+|	EXIM REPORT		 |
+|================================|
+
+Total mail sent: $c{"=>"}
+Total mail recieved $c{"<="}
+
+Total mail rejected: $c{" rejected"}
+Total mail unsolicited: $c{unsolicited}
+
+Number of times Google rate limited us: $c{"rate limited"}
+Numbet of mail rejected because it was in spamhaus.org list: $c{spamhaus}
+
+EOF
