e2e4cefb8e15305780fbcd220af00ebb37ae7a87
Al Biheiri <al@forgottheaddress.com>
2014-08-20 17:56:13 -0700

    enhacing frozen messages

--- eximparse.pl
+++ eximparse.pl
@@ -52,8 +52,9 @@ my $ip_adder  = qr{
 
 #variables use for the data collection
 my %c;
-my %received_addresses;
-my %deliver_addresses;
+my %hash_raddr;
+my %hash_daddr;
+my %message_ids;
 
 
 #creating the date variable
@@ -104,7 +105,7 @@ while (<>)
 			#if no ipaddr, its prob local so injecting local variable
 			$received_address ||= "local";
 			#lets add to our hash the info we got
-                        $received_addresses{$received_address}++;
+                        $hash_raddr{$received_address}++;
                     }
                 }
 
@@ -113,13 +114,27 @@ while (<>)
                     if (my ($deliver_address) = /=>.*?($ip_adder)/)
                     {
 			$deliver_address ||= "local";
-                        $deliver_addresses{$deliver_address}++;
+                        $hash_daddr{$deliver_address}++;
                     }
                 }
-
+	
+		#each message has an ID associated with it, capture uniq data and determine how many are frozen messages
+		if ($match =~ /[fF]rozen/)
+		{
+			if (my ($keys) = /.{6}\-.{6}\-.{2}/)
+			{
+				$message_ids{$keys}++;
+		
+			}
+		}
         }
 }
 
+#sum the message_ids
+my $unique_message_id_count = keys %message_ids;
+
+#part of reading the log file and determining what the last date of the log is
+#its placed here so that it is printed on screen after the whole log is parsed first (the while statement above).
 print "$ARGV ends on $last_date\n";
 
 #
@@ -142,7 +157,7 @@ print <<EOF;
 |-------------------------------|-----------------------|
 |mail recieved            	|$c{"<="}
 |-------------------------------|-----------------------|
-|frozen mail messages    	|$c{"frozen"}
+|frozen mail messages    	|$unique_message_id_count
 |-------------------------------|-----------------------|
 |unsolicited mail       	|$c{unsolicited}
 |-------------------------------|-----------------------|
@@ -164,9 +179,9 @@ EOF
 
 #sorting and organizing the exim top recieved ips
 #Putting b first will cause out to be sorted highest to lowest
-my @received_addr = sort { $received_addresses{$b} <=> $received_addresses{$a} } keys %received_addresses;
+my @received_addr = sort { $hash_raddr{$b} <=> $hash_raddr{$a} } keys %hash_raddr;
 #the print portion has a preceding "|" so it lines ip with the EOF chart 
-my $i; for my $item (@received_addr) { print  "|$received_addresses{$item} => $item\n"; last if ++$i == 20; }
+my $i; for my $item (@received_addr) { print  "|$hash_raddr{$item} => $item\n"; last if ++$i == 20; }
 
 print <<EOF;
 |							|
@@ -175,8 +190,8 @@ print <<EOF;
 |=======================================================|
 |Number of Times => IP Address				|
 EOF
-my @deliver_addr = sort { $deliver_addresses{$b} <=> $deliver_addresses{$a} } keys %deliver_addresses;
-my $d; for my $item (@deliver_addr) { print  "|$deliver_addresses{$item} => $item\n"; last if ++$d == 20; }
+my @deliver_addr = sort { $hash_daddr{$b} <=> $hash_daddr{$a} } keys %hash_daddr;
+my $d; for my $item (@deliver_addr) { print  "|$hash_daddr{$item} => $item\n"; last if ++$d == 20; }
 
 #return total time it took to run this script
 my ($user,$system,$cuser,$csystem) = times;
