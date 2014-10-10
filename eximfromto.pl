c8c0e888488daad39b3e42d02a2c6186acb120d5
Al Biheiri <al@forgottheaddress.com>
2014-10-10 11:49:54 -0700

    parses from,to, and subject line from exim maillog

---
+++ eximfromto.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+
+
+
+
+$input_file = "/Users/abiheiri/Downloads/transactions";
+open $input, "<", $input_file or die "could not open $input_file: $!";
+
+my %msgid;
+
+while(<$input>) {
+    ($month, $day, $time, $server, $proc, $id, $action, $rcpt) = split(/\s+/);
+
+    next unless $action eq '**';
+    next if $rcpt eq '<>';
+
+
+    $msgid{$id}->{'email'} = $rcpt;
+
+}
+
+seek $input, 0, 0;
+
+
+while(<$input>) {
+
+    for my $subject (/(\sT=".+")/g){
+
+        ($month, $day, $time, $server, $proc, $id, $action, $sender) = split(/\s+/);
+
+        next if $sender eq '<>';
+        next unless $action eq "<=";
+	print "$id [FROM]: $sender [TO]: ", $msgid{$id}->{'email'}, " [SUBJECT]: $subject\n";
+        $msgid{$id}->{'subject'} = $subject;
+        $msgid{$id}->{'sender'} = $sender;
+    }
+
+}
+
+#while ( my ($key, $value) = each(%msgid) ) {
+#    print "$key ", $value->{'email'},"\n";
+#}
+
